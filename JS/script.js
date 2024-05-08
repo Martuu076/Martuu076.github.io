97fd2c90ca7d9ead2bdd7d0b98c42329af7a108c
saporapo <somunoz@frba.utn.edu.ar>
2024-05-08 17:40:18 -0300

    arreglo de formalurio, falta q el texto del boton sea responsive

---
+++ JS/script.js
@@ -0,0 +1,8 @@
+function mostrarFormulario(){ 
+    var formulario = document.getElementById('Formulario'); 
+    formulario.style.visibility="visible";
+  }
+function cerrarFormulario(){
+  var formulario = document.getElementById('Formulario');
+  formulario.style.visibility="hidden";
+}
