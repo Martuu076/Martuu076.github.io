17037feebdf0a0ec03fbf30171c2fb16df024731
saporapo <somunoz@frba.utn.edu.ar>
2024-05-15 17:51:58 -0300

    despliegue de formulario arreglado

--- JS/script.js
+++ JS/script.js
@@ -1,8 +1,10 @@
 function mostrarFormulario(){ 
     var formulario = document.getElementById('Formulario'); 
     formulario.style.visibility="visible";
+    formulario.style.display="flex";
   }
 function cerrarFormulario(){
   var formulario = document.getElementById('Formulario');
   formulario.style.visibility="hidden";
+  formulario.style.display="none";
 }
